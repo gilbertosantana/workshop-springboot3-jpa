3651f128fc3e8a4f5e4fde92add5ee1c14a91b14
gilbertosantana <gilbertosantoss307@gmail.com>
2025-11-24 13:48:35 -0300

    OrderItem, many-to-many association with extra attributes PART 2

--- src/main/java/com/educandoweb/course/entities/pk/OrderItemPK.java
+++ src/main/java/com/educandoweb/course/entities/pk/OrderItemPK.java
@@ -8,7 +8,6 @@ import com.educandoweb.course.entities.Product;
 
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 
 @Embeddable
